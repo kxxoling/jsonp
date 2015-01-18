7214cdb1e4e9e7c7be35ca94bbed413af0a41910
kxxoling <kxxoling@gmail.com>
2015-01-18 21:16:27 +0800

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# 一个将 JSON 转化为 JSONP 的 Service
+
---
+++ app.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, request
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def jsonp_service():
+    callback = request.args.get('callback')
+    url = request.args.get('url')
+    orig_req = requests.get(url)
+    content = orig_req.content
+    return '%s(%s)' % (str(callback), content)      # callback 为 unicode 类型，需要先转换为 str 类型
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
